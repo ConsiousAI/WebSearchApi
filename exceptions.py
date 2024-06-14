af2e0df9f7ae326fdad8341e6d4df676b65e6cf5
Mystic_Ninja <159683183+sharry247@users.noreply.github.com>
2024-06-14 20:41:38 +0530

    Delete exceptions.py

--- exceptions.py
+++
@@ -1,7 +0,0 @@
-class APIError(Exception):
-    """Exception raised for errors in the API response."""
-    pass
-
-class InvalidQueryError(Exception):
-    """Exception raised for invalid queries."""
-    pass
